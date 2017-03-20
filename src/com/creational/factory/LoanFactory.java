d826a0b2dc59db3d79567ffa24baed32f9281510
funworks <harsh.sahajwani@gmail.com>
2017-03-20 23:53:53 +0530

    Updated factory implementation

--- src/com/creational/factory/LoanFactory.java
+++ src/com/creational/factory/LoanFactory.java
@@ -13,20 +13,4 @@ public class LoanFactory {
 		throw new IllegalArgumentException();
 	}
 
-	public static void main(String[] args) {
-		String loanType;
-		
-		loanType = "home";
-		Loan loan = LoanFactory.createLoan(loanType);
-		System.out.println("Home Loan interest = " + loan.calculateInterestAmount(1000));
-		
-		loanType = "education";
-		loan = LoanFactory.createLoan(loanType);
-		System.out.println("Education Loan interest = " + loan.calculateInterestAmount(1000));
-		
-		loanType = "personal";
-		loan = LoanFactory.createLoan(loanType);
-		System.out.println("Personal Loan interest = " + loan.calculateInterestAmount(1000));
-	}
-
 }
